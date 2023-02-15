0ebfdaacfad6555bfc9e7061d122211795b4ebab
liuliangcan <liuliang.can@163.com>
2023-02-15 19:19:35 +0800

    init

---
+++ SwitchNetwork.ps1
@@ -0,0 +1,14 @@
+﻿$PwshVersion=$PSVersionTable.PSVersion.Major
+$CMD="$PSHOME\powershell.exe"
+if($PwshVersion -gt 5)
+{
+    $CMD="$PSHOME\pwsh.exe"
+}
+$WorkPath=Split-Path -Parent $MyInvocation.MyCommand.Definition
+$WorkPwsh=Join-Path $WorkPath SwitchNetworkTool.ps1
+$WorkPwsh
+netsh interface show interface
+Start-Process $CMD  $WorkPwsh  -Verb runas -Wait
+# Start-Sleep –s 5
+netsh interface show interface
+Start-Sleep –s 5
---
+++ SwitchNetworkTool.ps1
@@ -0,0 +1,27 @@
+﻿
+$nets = @('WLAN','以太网')
+$a=netsh interface show interface
+$NetStatuss_array=$a|ConvertFrom-String
+# $NetStatuss_array
+
+foreach($NetStatus in $NetStatuss_array)
+{
+    if ($NetStatus.p4 -eq $nets[0]  )
+    {
+        
+        if($NetStatus.p1 -eq '已启用')
+        {   
+            '正在启用'+$nets[1]
+            netsh interface set interface $nets[1] enabled
+            '正在禁用'+$nets[0]
+            netsh interface set interface $nets[0] disabled
+        }
+        else {
+            '正在启用'+$nets[0]
+            netsh interface set interface $nets[0] enabled
+            '正在禁用'+$nets[1]
+            netsh interface set interface $nets[1] disabled
+        }
+    }
+    
+}
